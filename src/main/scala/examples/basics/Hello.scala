51808d5d9d61c74d1db3ef8abd5030405f2cbe9d
Daniel Suo <danielsuo@gmail.com>
2017-10-11 20:37:19 +0000

    Add hello world example

---
+++ src/main/scala/examples/basics/Hello.scala
@@ -0,0 +1,24 @@
+package examples.basics
+
+import fluent._
+import fluent.Rule.Infix
+import fluent.FluentActor.Message
+
+class Hello() extends FluentProgram {
+  override val name = "hello"
+  override val host = "localhost"
+  override val port = 8000
+
+  val stdout = new Stdout()
+
+  override val collections = List(stdout)
+  override val rules = List()
+}
+
+object Hello {
+  def main(args: Array[String]) = {
+    val hello = new Hello()
+    val (system, actor) = hello.run()
+    actor ! Message("stdout", Tuple1("Hello, world!"))
+  }
+}
